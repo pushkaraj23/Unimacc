587c5d1287887979dfc4c43b43772f3c0bcbdc9e
pushkaraj23 <pushkaraj232002@gmail.com>
2025-11-06 13:16:19 +0530

    changed url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://4.247.21.119:4798/api/",
+  baseURL: "https://api.unimacc.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
